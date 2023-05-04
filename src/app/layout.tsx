eb4fc14cd1680f1e8545b275c9e84d5ca8e7a990
Agshin Mammadov <107038254+agshinmammadov@users.noreply.github.com>
2023-05-04 13:21:02 +0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
 "use client"
 import './globals.css';
 import ProviderWrapper from '../redux/provider';
-<<<<<<< HEAD
-
-=======
->>>>>>> 5850c2d0fefd76bc0afa0250dd37dcf0f1a31094
 export default function RootLayout({
   children
 }: {
