3cf1c88986b997b6919263718f7387b5495c53df
Agshin Mammadov <agshinmammadov@yandex.com>
2023-05-13 13:29:41 +0400

    localstorage
    localStorage is not defined problem is solved

--- redux/store.tsx
+++ redux/store.tsx
@@ -3,37 +3,38 @@ import { rootReducer } from './reducers/rootReducer';
 import { useEffect } from 'react';
 
 export interface RootState { 
-  // Define your state properties here
 }
-
 const configureStore = (): Store<RootState> => {
   let initialState = {};
 
-  try {
-    const persistedState = localStorage.getItem('reduxState');
-    if (persistedState) {
-      initialState = JSON.parse(persistedState);
+  // Check if the code is being executed on the client
+  if (typeof window !== 'undefined') {
+    try {
+      const persistedState = localStorage.getItem('reduxState');
+      if (persistedState) {
+        initialState = JSON.parse(persistedState);
+      }
+    } catch (error) {
+      console.log(`"There problem in redux store. Error name:" ${error}`)
     }
-  } catch (error) {
-    console.log(`"There problem in redux store. Error name:" ${error}`)
   }
 
   const store = createStore(rootReducer, initialState);
 
-  useEffect(() => {
-    if (typeof window !== 'undefined' && window.localStorage) {
-      store.subscribe(() => {
-        const state = store.getState();
-        try {
-          localStorage.setItem('reduxState', JSON.stringify(state));
-        } catch (error) {
-          console.log(`"There is problem with sending data to localstorage. Error name:" ${error}`)
-        }
-      });
-    }
-  }, [store]);
+  // Save the state to localStorage whenever it changes, but only on the client
+  if (typeof window !== 'undefined') {
+    store.subscribe(() => {
+      const state = store.getState();
+      try {
+        localStorage.setItem('reduxState', JSON.stringify(state));
+      } catch (error) {
+        console.log(`"There is problem with sending data to localstorage. Error name:" ${error}`)
+      }
+    });
+  }
 
   return store;
 };
+
 
 export default configureStore;
